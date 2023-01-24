5b6a97dc8357d09223c322f8fbf76b8727e86e24
BIPESH KHADKA10 <106030583+Bipeshkhadka10@users.noreply.github.com>
2023-01-24 18:24:51 +0545

    Add files via upload

---
+++ 01_operator_overloading.cpp
@@ -0,0 +1,39 @@
+// prefix unary operator overloading ++
+#include<iostream>
+using namespace std;
+
+class counter{
+     int count;
+     public:
+      counter()
+      {
+          count=0;
+      }
+      void show_count()
+      {
+          cout<<count;
+      }
+      void operator ++();
+};
+void counter::operator++()
+{
+     ++count;
+}
+
+int main()
+{
+     counter c1, c2;
+     cout<<"c1=";
+     c1.show_count();
+     cout<<"c2=";
+     c2.show_count();
+     ++c1;
+     ++c2;
+     ++c2;
+     cout<<endl<<"after operator overloading"<<endl;
+     cout<<"c1=";
+     c1.show_count();
+     cout<<"c2=";
+     c2.show_count();
+     return 0;
+}
